cb37c478241cd30615b7b55afd2fadbb6aeb7457
garretthilberling <ghilberling@knights.ucf.edu>
2022-04-22 20:33:49 -0400

    reaction routes are done

--- server.js
+++ server.js
@@ -119,8 +119,8 @@ app.get('/api/thoughts', (req, res) => {
 });
 
 // get single thought
-app.get('/api/thought/:id', ({ params, body }, res) => {
-  User.findOne({ _id: params.id })
+app.get('/api/thoughts/:id', ({ params, body }, res) => {
+  Thought.findOne({ _id: params.id })
   .populate({
     path: 'reactions',
     select: '-__v'
@@ -191,4 +191,29 @@ app.put('/api/thoughts/:id', ({ params, body }, res) => {
 });
 
 // create reaction
-app.post
+app.post('/api/thoughts/:thoughtId/reactions', ({ params, body }, res) => {
+  Thought.findOneAndUpdate(
+    { _id: params.thoughtId },
+    { $push: { reactions: body } },
+    { new: true, runValidators: true }
+  )
+  .then(dbThoughtData => {
+    if(!dbThoughtData) {
+      res.status(404).json({ message: 'No thought data found with this id!' });
+      return
+    }
+    res.json(dbThoughtData);
+  })
+  .catch(err => res.json(err));
+});
+
+// delete reaction
+app.delete('/api/thoughts/:thoughtId/reactions/:reactionId', ({ params, body }, res) => {
+  Thought.findOneAndUpdate(
+    { _id: params.thoughtId },
+    { $pull: { reactions: { reactionId: params.reactionId } } },
+    { new: true }
+  )
+  .then(dbThoughtData => res.json(dbThoughtData))
+  .catch(err => res.json(err));
+});
